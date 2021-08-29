43cd171b9568cb66977c9e2e3240a8f3583fda7c
Ahmed <a.alkamel25@gmail.com>
2021-08-29 22:54:50 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// var delay =1000;
+// function autoSlider(){
+//     $('.slider .active').each(function(){
+//         if(!$(this).is(':last-child')){
+//             $(this).delay(delay).fadeOut(400, function(){
+//                 $(this).removeClass('active').next().addClass('active').fadeIn(400);
+//             })
+//         }else{
+//             $(this).delay(delay).fadeOut(400, function(){
+//                 $(this).removeClass('active');
+//                 $('.slider div').eq(0).addClass('active').fadeIn(400);
+//             },
+//             )
+//         }
+//     })
+// }
+
+// autoSlider()
