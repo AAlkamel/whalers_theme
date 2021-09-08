8644fc92632a57587149a5915d05e43d9ec6e60e
Ahmed <a.alkamel25@gmail.com>
2021-09-08 21:14:48 +0300

    add01

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,3 +16,26 @@
 // }
 
 // autoSlider()
+// ---------------- mobile menu ------------------
+$('.nave .menu').on('click',()=>{
+    $('.mobile-menu').toggleClass('active');
+})
+// ---------------- purchases bag ------------------
+$('.purchases-btn').on('click',()=>{
+    $('.purchases').addClass('active');
+})
+$('.purchases .head span.close ').on('click',()=>{
+    $('.purchases').removeClass('active');
+})
+
+// -------------like--------
+$( ".fba.like" ).click(function() {
+    $( this ).toggleClass( "heart" );
+});
+// -------------i.ti-layout list or grid--------
+$( "i.ti-layout-grid3" ).click(function() {
+    $( '.items' ).removeClass( "list" );
+});
+$( "i.ti-view-list-alt" ).click(function() {
+    $( '.items' ).addClass( "list" );
+});
