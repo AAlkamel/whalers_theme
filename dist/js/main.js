d5733de83cdd5f9b3d1e286add95be84afe062a2
Ahmed <a.alkamel25@gmail.com>
2021-09-17 10:02:37 +0300

    finish

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,26 +16,58 @@
 // }
 
 // autoSlider()
-// ---------------- mobile menu ------------------
-$('.nave .menu').on('click',()=>{
-    $('.mobile-menu').toggleClass('active');
-})
-// ---------------- purchases bag ------------------
-$('.purchases-btn').on('click',()=>{
-    $('.purchases').addClass('active');
-})
-$('.purchases .head span.close ').on('click',()=>{
-    $('.purchases').removeClass('active');
-})
+$(document).ready(function() {
+    // ---------------- mobile menu ------------------
+    $('.nave .menu').on('click',()=>{
+        $('.mobile-menu').toggleClass('active');
+    })
+    // ---------------- purchases bag ------------------
+    $('.purchases-btn').on('click',()=>{
+        $('.purchases').addClass('active');
+    })
+    $('.purchases .head span.close ').on('click',()=>{
+        $('.purchases').removeClass('active');
+    })
 
-// -------------like--------
-$( ".fba.like" ).click(function() {
-    $( this ).toggleClass( "heart" );
-});
-// -------------i.ti-layout list or grid--------
-$( "i.ti-layout-grid3" ).click(function() {
-    $( '.items' ).removeClass( "list" );
-});
-$( "i.ti-view-list-alt" ).click(function() {
-    $( '.items' ).addClass( "list" );
-});
+    // -------------like--------
+    $( ".fba.like" ).click(function() {
+        $( this ).toggleClass( "heart" );
+    });
+    // -------------like--------
+    $( "i.ti.ti-heart" ).click(function() {
+        $( this ).toggleClass( "heart" );
+    });
+    // -------------i.ti-layout list or grid--------
+    $( "i.ti-layout-grid3" ).click(function() {
+        $( '.items' ).removeClass( "list" );
+    });
+    $( "i.ti-view-list-alt" ).click(function() {
+        $( '.items' ).addClass( "list" );
+    });
+
+    // ------------- My Account ------------------------
+    $('#side-btn li').click(function(){
+        var id = $(this).attr("id");
+        var cla = '.'+ id
+        var i = $(this).text();
+        $('#title').text(i)
+        $('.forms > form').css("display", "none")
+        $(cla).css("display", "block")
+        
+        // console.log(id);
+    })
+    // ------------- popups ------------------------
+    $('#open-popup01').click(function(){
+        $('#popup01').slideDown();
+        
+    })
+    $('#open-popup02').click(function(){
+        $('#popup02').slideDown();
+        
+    })
+    $('.close').click(function(){
+        $('.popup').slideUp();
+        
+    })
+
+})
